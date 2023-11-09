ecbcbfa2338520d1abf7f279f7ae057233d39d10
Dash <Promise.Dash@chubb.com>
2023-11-09 19:30:18 +0530

    initial commit

---
+++ Student_Management/Controllers/StudentsController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Student_Management.Models;
+using Student_Management.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Student_Management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IStudentService studentService;
+        private readonly MongoClient mongoClient;
+
+        public StudentsController(IStudentService studentService)
+        {
+            this.studentService = studentService;
+            this.mongoClient = mongoClient;
+        }
+        // GET: api/<StudentsController>
+        [HttpGet]
+        public ActionResult<List<Student>> Get()
+        {
+            return studentService.Get();
+        }
+
+        // GET api/<StudentsController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Student> Get(string id)
+        {
+            var student = studentService.Get(id);
+
+            if (student == null)
+            {
+                return NotFound($"Student with Id = {id} not found");
+            }
+
+            return student;
+        }
+
+        // POST api/<StudentsController>
+        [HttpPost]
+        public ActionResult<Student> Post([FromBody] Student student)
+        {
+            student.Id = ObjectId.GenerateNewId().ToString();
+            studentService.Create(student);
+            return CreatedAtRoute(nameof(Get), new { id = student.Id }, student);
+        }
+
+        // PUT api/<StudentsController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, [FromBody] Student student)
+        {
+            var existingStudent = studentService.Get(id);
+
+            if (existingStudent == null)
+            {
+                return NotFound($"Student with Id = {id} not found");
+            }
+
+            ObjectId objectId = new ObjectId(id);
+            student.Id = objectId.ToString();
+
+            studentService.Update(id, student);
+
+            return NoContent();
+        }
+
+        // DELETE api/<StudentsController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            var student = studentService.Get(id);
+
+            if (student == null)
+            {
+                return NotFound($"Student with Id = {id} not found");
+            }
+
+            studentService.Remove(student.Id);
+
+            return Ok($"Student with Id = {id} deleted");
+        }
+    }
+}
---
+++ Student_Management/Services/StudentService.cs
@@ -0,0 +1,60 @@
+﻿using MongoDB.Driver;
+using Student_Management.Models;
+
+namespace Student_Management.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IMongoCollection<Student> _students;
+        private IMongoClient? mongoClient;
+        private IStudentStoreDatabaseSettings? studentStoreDatabaseSettings;
+
+        public StudentService(IStudentStoreDatabaseSettings settings, MongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _students = database.GetCollection<Student>(settings.StudentCoursesCollectionName);
+        }
+
+        public StudentService(IMongoClient mongoClient, IStudentStoreDatabaseSettings studentStoreDatabaseSettings)
+        {
+            this.mongoClient = mongoClient;
+            this.studentStoreDatabaseSettings = studentStoreDatabaseSettings;
+            var database = mongoClient.GetDatabase(studentStoreDatabaseSettings.DatabaseName);
+            _students = database.GetCollection<Student>(studentStoreDatabaseSettings.StudentCoursesCollectionName);
+        }
+
+        public Student Create(Student student)
+        {
+            try
+            {
+                _students.InsertOne(student);
+            }
+            catch (MongoDB.Bson.BsonSerializationException ex)
+            {
+                var message = $"Error creating student with Id '{student.Id}': {ex.Message}";
+                throw new ArgumentException(message, ex);
+            }
+            return student;
+        }
+
+        public List<Student> Get()
+        {
+            return _students.Find(student => true).ToList();
+        }
+
+        public Student Get(string id)
+        {
+            return _students.Find(student => student.Id == id).FirstOrDefault();
+        }
+
+        public void Remove(string id)
+        {
+            _students.DeleteOne(student => student.Id == id);
+        }
+
+        public void Update(string id, Student student)
+        {
+            _students.ReplaceOne(student => student.Id == id, student);
+        }
+    }
+}
